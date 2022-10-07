008adb2c53b4a0234826f14c8d2ae9f29fb6df4c
herdyhardiyant <54350341+herdyhardiyant@users.noreply.github.com>
2022-10-07 08:13:21 +0700

    Player Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
+using DG.Tweening.Core;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform _playerTransform;
 
-    // Update is called once per frame
+    [SerializeField] private float _moveDuration = .5f;
+    [SerializeField] private float _rotateDuration = .2f;
+
     void Update()
     {
+        var playerCurrentPosition = _playerTransform.position;
         
+        if (Input.GetKeyDown("w"))
+        {
+            _playerTransform.DOMoveZ(playerCurrentPosition.z + 1, _moveDuration);
+            _playerTransform.DORotate(new Vector3(0, 0, 0), _rotateDuration);
+        }
+        else if (Input.GetKeyDown("s"))
+        {
+            _playerTransform.DOMoveZ(playerCurrentPosition.z + -1, _moveDuration);
+            _playerTransform.DORotate(new Vector3(0, 180, 0), _rotateDuration);
+        }
+        else if (Input.GetKeyDown("a"))
+        {
+            _playerTransform.DOMoveX(playerCurrentPosition.x + -1, _moveDuration);
+            _playerTransform.DORotate(new Vector3(0, -90, 0), _rotateDuration);
+        }
+        else if (Input.GetKeyDown("d"))
+        {
+            _playerTransform.DOMoveX(playerCurrentPosition.x + 1, _moveDuration);
+            _playerTransform.DORotate(new Vector3(0, 90, 0), _rotateDuration);
+        }
     }
 }
